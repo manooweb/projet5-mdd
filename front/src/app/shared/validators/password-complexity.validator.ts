49aa8533a7e0bab57b8de236f2629d3190d6653b
Emmanuel Hesry <ehesry@gmail.com>
2026-08-29 16:20:11 +0200

    refactor(front): use String.raw for password strength regex

--- front/src/app/shared/validators/password-complexity.validator.ts
+++ front/src/app/shared/validators/password-complexity.validator.ts
@@ -2,7 +2,7 @@ import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 const PASSWORD_COMPLEXITY = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])/;
 
-export const passwordStrengthRegex = '^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,})';
+export const passwordStrengthRegex = String.raw`^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^A-Za-z0-9])(?=.{8,})`;
 
 export const passwordComplexityValidator: ValidatorFn = (
   control: AbstractControl,
